cc5b704254b000e26a8f280cb227c4f19b677fe9
JorjDominic <03jorgedmf12@gmail.com>
2026-08-09 12:08:24 +0800

    supabase project transfer and fix

--- lib/core/config/supabase_config.dart
+++ lib/core/config/supabase_config.dart
@@ -1,9 +1,6 @@
 class SupabaseConfig {
-  static const String url = String.fromEnvironment(
-    'NEXT_PUBLIC_SUPABASE_URL=https://crkbhffzzwdaccprgojj.supabase.co',
-  );
+  static const String url = 'https://smlgnwnbuhruprlynfuw.supabase.co';
 
-  static const String publishableKey = String.fromEnvironment(
-    'NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_8lNEiLcpz95__yx5ru4v9A_ZgV7rPtd',
-  );
+  static const String publishableKey =
+      'sb_publishable_RwYTcl77cuUgq7OZ7peKkA_U_bt0sh7';
 }
